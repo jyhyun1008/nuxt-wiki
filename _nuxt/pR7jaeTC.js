2235bdba1741abcab6e4e5b918c3a312b0a5de37
jyhyun1008 <jayhyun1008@gmail.com>
2025-11-22 11:30:34 +0900

    Updates

---
+++ _nuxt/pR7jaeTC.js
@@ -0,0 +1,1 @@
+import{r as n}from"./Bc_Cdcze.js";import a from"./ChP0wAWh.js";import{a0 as s,b as t,o as e,v as i,y as p}from"./C0wJ9ymL.js";import"./BN_7HF1G.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,f)=>(e(),t(a,{color:"warning",icon:p(o).ui.icons.warning},{default:i(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
