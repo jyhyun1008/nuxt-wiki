70f3276ba3078ab6f699850dad33ff58d3898c32
jyhyun1008 <jayhyun1008@gmail.com>
2025-11-23 15:56:33 +0900

    Updates

--- _nuxt/T-s440KI.js
+++
@@ -1,1 +0,0 @@
-import{r as n}from"./Dp7LipX2.js";import a from"./wegADfHT.js";import{a0 as s,b as t,o as e,v as i,y as p}from"./5WByRL5i.js";import"./BN_7HF1G.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,f)=>(e(),t(a,{color:"warning",icon:p(o).ui.icons.warning},{default:i(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
