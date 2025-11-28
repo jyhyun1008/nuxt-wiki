605e73e0686eb4ce1e5862d6462614ba4f9a34b0
jyhyun1008 <jayhyun1008@gmail.com>
2025-11-28 11:09:08 +0900

    Updates

--- _nuxt/Cpf9ccO-.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./CjeDpLgI.js";import a from"./DLbzPGo5.js";import{a0 as e,b as n,o as s,v as i,y as p}from"./DfVMgGzo.js";import"./BN_7HF1G.js";const d={__name:"ProseNote",setup(c){const o=e();return(r,f)=>(s(),n(a,{color:"info",icon:p(o).ui.icons.info},{default:i(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
