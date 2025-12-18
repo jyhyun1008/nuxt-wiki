9ca9c1063b3ef4f3bc984d30d6baf113a250a562
jyhyun1008 <jayhyun1008@gmail.com>
2025-12-18 11:29:17 +0900

    Updates

---
+++ _nuxt/DmW4U2Id.js
@@ -0,0 +1,1 @@
+import{r as a}from"./CjsC3G61.js";import t from"./QiCqlQOT.js";import{a0 as e,b as n,o as s,v as c,y as i}from"./OGtTzRzo.js";import"./BN_7HF1G.js";const d={__name:"ProseCaution",setup(p){const o=e();return(r,f)=>(s(),n(t,{color:"error",icon:i(o).ui.icons.caution},{default:c(()=>[a(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
